470adddbc7572a659d721e00cf664d97a9377fc3
Joshua Jolley <jolleyboy@gmail.com>
2023-12-09 12:08:07 -0700

    Day 5 part 1

---
+++ rust/2023/src/day5.rs
@@ -0,0 +1,101 @@
+use std::collections::BTreeMap;
+
+pub struct Almanac {
+    seeds: Vec<u32>,
+    maps: Vec<BTreeMap<u32, [u32; 2]>>
+}
+
+impl Almanac {
+    fn traverse (&self, seed: &u32) -> u32 {
+        let mut val = seed.clone();
+
+        println!("Num maps: {}", self.maps.len());
+
+        for i in 0..self.maps.len() {
+            // Find the highest key below val and see if we're within the offset
+            val = match self.maps[i].range(0..=val).next_back() {
+                Some(x) => {
+                    let source_start = x.0;
+                    let [dest_start, offset] = x.1;
+                    let diff = val - source_start;
+                    if diff < *offset {
+                        // We have a mapping!
+                        dest_start + diff
+                    } else {
+                        // No mapping here :(
+                        val
+                    }
+
+                },
+                None => val
+            }
+        }
+
+        return val;
+    }
+}
+
+#[aoc_generator(day5)]
+pub fn input_generator(input: &str) -> Almanac {
+    let mut maps = Vec::new();
+    let mut lines = input.lines();
+    let seeds = lines.next()
+        .unwrap()
+        .split_whitespace()
+        .skip(1)
+        .map(|s| s.parse::<u32>().unwrap())
+        .collect::<Vec<_>>();
+
+    // drop the empty line and first map title
+    lines.next();
+    lines.next();
+
+    let mut map: BTreeMap<u32, [u32;2]> = BTreeMap::new();
+    let mut map_i = 0;
+
+    lines.for_each(|line: &str| {
+        let first_char = line.chars().nth(0);
+        match first_char {
+            Some(ch) => {
+                // If it's a number, it's part of the next set
+                match ch {
+                    c if c.is_ascii_digit() => {
+                        let mut parts = line.split_whitespace().map(|n| n.parse::<u32>().unwrap());
+                        let dest_start = parts.next().unwrap();
+                        let source_start = parts.next().unwrap();
+                        let range_size = parts.next().unwrap();
+
+                        println!("source_start: {}", source_start);
+                        println!("dest_start: {}", dest_start);
+                        println!("range_size: {}", range_size);
+                        map.insert(source_start, [dest_start, range_size]);
+                    }
+                    // Anything else we don't care about
+                    _ => ()
+                }
+            }
+            // Empty lines signify the end of input
+            None => {
+                println!("");
+                println!("Saving old map #{} and making a new map!", map_i);
+                println!("");
+                maps.insert(map_i, map.clone());
+                map_i += 1;
+                map = BTreeMap::new()
+            }
+        };
+    });
+
+    // Add the last map
+    maps.insert(map_i, map.clone());
+
+    Almanac {
+        seeds,
+        maps
+    }
+
+}
+#[aoc(day5, part1)]
+pub fn solve_part1(almanac: &Almanac) -> u32 {
+    almanac.seeds.iter().map(|seed| almanac.traverse(seed)).min().unwrap()
+}
